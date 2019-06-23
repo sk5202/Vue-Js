03f0f0521d8233da6ad555739da618f2472b1fa3
Smiten Kalathiya <smitenkalathiya@gmail.com>
2019-06-23 22:19:15 +0530

    Add sample project

---
+++ VueDemoWithAsp.NetCore/VueDemoWithAsp.NetCore/VueCoreConnection/Connection.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SpaServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace VueDemoWithAsp.NetCore.VueCoreConnection
+{
+    public static class Connection
+    {
+       
+        private static int Port { get; } = 8080;
+        private static Uri DevelopmentServerEndpoint { get; } = new Uri($"http://localhost:{Port}");
+        private static TimeSpan Timeout { get; } = TimeSpan.FromSeconds(60);
+       
+        private static string DoneMessage { get; } = "DONE  Compiled successfully in";
+
+        public static void UseVueDevelopmentServer(this ISpaBuilder spa)
+        {
+            spa.UseProxyToSpaDevelopmentServer(async () =>
+            {
+                var loggerFactory = spa.ApplicationBuilder.ApplicationServices.GetService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger("Vue");
+
+                if (IsRunning())
+                {
+                    return DevelopmentServerEndpoint;
+                }
+
+              
+                var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+                var processInfo = new ProcessStartInfo
+                {
+                    FileName = isWindows ? "cmd" : "npm",
+                    Arguments = $"{(isWindows ? "/c npm " : "")}run serve",
+                    WorkingDirectory = "client-app",
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                };
+                var process = Process.Start(processInfo);
+                var tcs = new TaskCompletionSource<int>();
+                _ = Task.Run(() =>
+                {
+                    try
+                    {
+                        string line;
+                        while ((line = process.StandardOutput.ReadLine()) != null)
+                        {
+                            logger.LogInformation(line);
+                            if (!tcs.Task.IsCompleted && line.Contains(DoneMessage))
+                            {
+                                tcs.SetResult(1);
+                            }
+                        }
+                    }
+                    catch (EndOfStreamException ex)
+                    {
+                        logger.LogError(ex.ToString());
+                        tcs.SetException(new InvalidOperationException("'npm run serve' failed.", ex));
+                    }
+                });
+                _ = Task.Run(() =>
+                {
+                    try
+                    {
+                        string line;
+                        while ((line = process.StandardError.ReadLine()) != null)
+                        {
+                            logger.LogError(line);
+                        }
+                    }
+                    catch (EndOfStreamException ex)
+                    {
+                        logger.LogError(ex.ToString());
+                        tcs.SetException(new InvalidOperationException("'npm run serve' failed.", ex));
+                    }
+                });
+
+                var timeout = Task.Delay(Timeout);
+                if (await Task.WhenAny(timeout, tcs.Task) == timeout)
+                {
+                    throw new TimeoutException();
+                }
+
+                return DevelopmentServerEndpoint;
+            });
+
+        }
+
+        private static bool IsRunning() => IPGlobalProperties.GetIPGlobalProperties()
+                .GetActiveTcpListeners()
+                .Select(x => x.Port)
+                .Contains(Port);
+    }
+}
---
+++ VueDemoWithAsp.NetCore/VueDemoWithAsp.NetCore/client-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="newapp">
+    <img alt="Vue logo" id="newlogo" src="./assets/dotnetcore-vue.jpg">
+      <input type="hidden" id="hintidentifier" value="dotnetcore"/>
+    <HelloWorld msg="This is my first vue application"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+    .newapp{
+        text-align:center;
+        margin-top:50px;    
+    }
+</style>
